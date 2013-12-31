af4002fd17ecf1cac03394ce8b1ce30b5fb824cf
Anenth Guru <anenth@ideophone.in>
2014-01-01 04:19:05 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-airtel-chrome-extension
+act-chrome-extension
 ===
 
-Shows your Airtel data usage, if you are on an Airtel broadband connection in India.
+Shows your ACT Broadband data usage.
+
+Based on 'airtel-chrome-extension' by Rakesh Pai. https://github.com/rakeshpai/airtel-chrome-extension
 
 Installation
 ---
 
-1. [Download the zip](https://github.com/rakeshpai/airtel-chrome-extension/archive/master.zip) and extract it into a suitable folder on your hard drive.
+1. [Download the zip](https://github.com/anenthg/act-chrome-extension/archive/master.zip) and extract it into a suitable folder on your hard drive.
 2. Type in `chrome://extensions` in your address bar in Google Chrome.
 3. Check the "Developer mode" checkbox at the top right of the page.
 4. Click the "Load unpacked extension..." button, and select the folder that you created in the first step.
 
 That's it! You should see a new Airtel logo next to your Chrome address bar. Click it to see details about your data usage.
+-----
 
-Why isn't this on the Chrome Web Store?
----
-
-Because Chrome has started charging $5 as "[developer registration](https://support.google.com/chrome_webstore/answer/187591?hl=en&ref_topic=1212368)". I can't be bothered to either register or to spend $5.
 
 License
 ---
--- popup.js
+++ popup.js
@@ -1,44 +1,59 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://122.160.230.125:8080/gbod/gb_on_demand.do", true);
-xhr.onreadystatechange = function() {
-	if(xhr.readyState != 4) return;
+var list = [
+    {'name':'ACT Freedom','data':'20 GB'},
+    {'name':'ACT Liberty','data':'30 GB'},
+    {'name':'ACT Privilege','data':'40 GB'},
+    {'name':'ACT Abundant','data':'50 GB'},
+    {'name':'ACT Indulge','data':'60 GB'},
+    {'name':'ACT Extravagant','data':'100 GB'},
+    {'name':'ACT Force','data':'200 GB'},
+    {'name':'ACT BB HO Value','data': '250 GB'},    
+    {'name':'ACT BB HO Extra','data': '350 GB'}
+];
+var xhr1 = new XMLHttpRequest();
+xhr1.open("GET", "http://portal.acttv.in/index.php/mypackage", true);
+xhr1.onreadystatechange = function() {
+	if(xhr1.readyState != 4) return;
 
 	document.body.classList.remove("loading");
 
-	if(xhr.status != 200) {
+	if(xhr1.status != 200) {
 		document.body.classList.add("error");
 		return;
 	}
 
 	var div = document.createElement("div");
-	div.innerHTML = xhr.responseText;
+	div.innerHTML = xhr1.responseText;
 
-	var ul = div.querySelector("#container .content ul");
-	var items = Array.prototype.slice.apply(ul.getElementsByTagName("li"));
+	var fup = document.getElementById("fup");
+	var value = div.querySelector(".moduletable tr:nth-child(3) td:nth-child(3)").textContent.trim();
 
-	items = items.map(function(item) {
-		return item.innerText.split(":")[1].trim();
-	});
-
-	var remaining = parseFloat(items[1]),
-		total = parseFloat(items[2]),
-		remainingDays = items[3];
-
-	var template = document.querySelector("#detailsTemplate").content.cloneNode(true);
-	template.querySelector("meter").setAttribute("max", total);
-	template.querySelector("meter").setAttribute("value", total - remaining);
-	template.querySelector("meter").setAttribute("high", 0.8 * total);
-	template.querySelector(".percentage").innerText = template.querySelector(".percentage").innerText.replace("${used}", ((total - remaining)*100/total).toFixed(2));
-	var gist = template.querySelector(".gist").innerHTML;
-	gist = gist.replace("${remaining}", remaining);
-	gist = gist.replace("${days}", remainingDays);
-	template.querySelector(".gist").innerHTML = gist;
-	template.querySelector("footer").innerHTML = template.querySelector("footer").innerHTML.replace("${dslNumber}", items[0]);
-
-
-	document.querySelector("#details").appendChild(template);
-
-	document.body.classList.add("details");
+	for (vendor in list){
+	    if(value == list[vendor].name.toUpperCase()){
+	    	fup.innerHTML = list[vendor].data;
+	    	break;
+	    }
+	}
 };
+xhr1.send();
 
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://portal.acttv.in/index.php/myusage", true);
+xhr.onreadystatechange = function() {
+
+	var div = document.createElement("div");
+	div.innerHTML = xhr.responseText;
+	
+	consumed = document.getElementById("consumed");
+	consumed.innerHTML = div.querySelector("#total td:nth-child(2)").textContent;
+
+	var fup = document.getElementById("fup");
+
+	var fup_no = fup.innerHTML.replace(" GB","");
+	var consumed_no = consumed.innerHTML.replace("&nbsp;GB","");
+	bb_meter = document.getElementById("bb-meter");
+
+	bb_meter.innerHTML =  ((fup_no - consumed_no) / fup_no)*100 + "%";
+};
 xhr.send();
