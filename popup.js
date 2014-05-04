502ed987ee4db64183047f57b988377f52596ae2
Jaseem Abid <jaseemabid@gmail.com>
2014-05-04 18:27:52 +0530

    Fix white space, indent to 4 spaces throughout
    Also define a few undefined variables

--- popup.js
+++ popup.js
@@ -6,52 +6,57 @@ var list = [
     {'name':'ACT Indulge','data':'60 GB'},
     {'name':'ACT Extravagant','data':'100 GB'},
     {'name':'ACT Force','data':'200 GB'},
-    {'name':'ACT BB HO Value','data': '250 GB'},    
+    {'name':'ACT BB HO Value','data': '250 GB'},
     {'name':'ACT BB HO Extra','data': '350 GB'}
 ];
 
 var xhr1 = new XMLHttpRequest();
 xhr1.open("GET", "http://portal.acttv.in/index.php/mypackage", true);
-xhr1.onreadystatechange = function() {
-	if(this.readyState != 4) return;
+xhr1.onreadystatechange = function () {
+    if (this.readyState != 4) return;
 
-	document.body.classList.remove("loading");
+    document.body.classList.remove("loading");
 
-	if(this.status != 200) {
-		document.body.classList.add("error");
-		return;
-	}
+    if (this.status !== 200) {
+        document.body.classList.add("error");
+        return;
+    }
 
-	var div = document.createElement("div");
-	div.innerHTML = this.responseText;
+    var div = document.createElement("div");
+    div.innerHTML = this.responseText;
 
-	var fup = document.getElementById("fup");
-	var value = div.querySelector(".moduletable tr:nth-child(3) td:nth-child(3)").textContent.trim();
-
-	for (vendor in list){
-	    if(value == list[vendor].name.toUpperCase()){
-	    	fup.innerHTML = list[vendor].data;
-	    	break;
-	    }
-	}
+    var fup = document
+            .getElementById("fup");
+    var value = div
+            .querySelector(".moduletable tr:nth-child(3) td:nth-child(3)")
+            .textContent
+            .trim();
+    var vendor;
+    for (vendor in list) {
+        if (value == list[vendor].name.toUpperCase()) {
+            fup.innerHTML = list[vendor].data;
+            break;
+        }
+    }
 };
 xhr1.send();
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://portal.acttv.in/index.php/myusage", true);
-xhr.onreadystatechange = function() {
-	var div = document.createElement("div");
-	div.innerHTML = this.responseText;
+xhr.onreadystatechange = function () {
+    var div = document.createElement("div");
+    div.innerHTML = this.responseText;
+    var consumed;
+    consumed = document.getElementById("consumed");
+    consumed.innerHTML = div.querySelector("#total td:nth-child(2)").textContent;
 
-	consumed = document.getElementById("consumed");
-	consumed.innerHTML = div.querySelector("#total td:nth-child(2)").textContent;
+    var fup = document.getElementById("fup");
 
-	var fup = document.getElementById("fup");
+    var fup_no = fup.innerHTML.replace(" GB", "");
+    var consumed_no = consumed.innerHTML.replace("&nbsp;GB", "");
+    var bb_meter;
+    bb_meter = document.getElementById("bb-meter");
 
-	var fup_no = fup.innerHTML.replace(" GB","");
-	var consumed_no = consumed.innerHTML.replace("&nbsp;GB","");
-	bb_meter = document.getElementById("bb-meter");
-
-	bb_meter.innerHTML =  ((fup_no - consumed_no) / fup_no)*100 + "%";
+    bb_meter.innerHTML = ((fup_no - consumed_no) / fup_no) * 100 + "%";
 };
 xhr.send();
